ed2fe7f9ed1669995ce061664b6a5df393b12783
Dave Trimboli <davidatrimboli@gmail.com>
2020-08-25 02:28:06 -0400

    seed changes

--- sql/seed.sql
+++ sql/seed.sql
@@ -5,12 +5,12 @@ VALUES ('Sales'), ('Development');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Rep', 60000, 1),
-  ('Sales Manager', 100000, 1),
-  ('Jr Developer', 60000, 2),
-  ('Sr Developer', 100000, 2);
+  ('Sales Manager', 90000, 1),
+  ('Jr Developer', 75000, 2),
+  ('Sr Developer', 120000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 2, NULL),
-  ('Jane', 'Doe', 1, 1),
-  ('Jack', 'Doe', 4, NULL),
-  ('James', 'Doe', 3, 3);
+VALUES ('Jenna', 'Fischer', 2, NULL),
+  ('Steve', 'Carell', 1, 1),
+  ('John', 'Krasinski', 4, NULL),
+  ('Craig', 'Robinson', 3, 3);
